3244baad7f69b867340b170a797449b508fc9bbd
Heiko Stuebner <heiko.stuebner@theobroma-systems.com>
2019-12-09 15:31:26 +0100

    drm/bridge/synopsys: dsi: move phy_ops callbacks around panel enablement
    If implementation-specific phy_ops need to be defined they probably
    should be enabled before trying to talk to the panel and disabled only
    after the panel was disabled.
    
    Right now they are enabled last and disabled first, so might make it
    impossible to talk to some panels - example for this being the px30
    with an external Innosilicon dphy that needs the phy to be enabled
    to transfer commands to the panel.
    
    So move the calls appropriately.
    
    changed in v5:
    - rebased on top of 5.5-rc1
    - merged with dsi timing change to prevent ordering conflicts
    
    Upstream-Status: Backport [89bddff6be745fd688bbdd53773f5eb3136051eb]
    Signed-off-by: Heiko Stuebner <heiko.stuebner@theobroma-systems.com>
    Reviewed-by: Neil Armstrong <narmstrong@baylibre.com>
    Tested-by: Yannick Fertre <yannick.fertre@st.com>
    Reviewed-by: Philippe Cornu <philippe.cornu@st.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20191209143130.4553-3-heiko@sntech.de

--- drivers/gpu/drm/bridge/synopsys/dw-mipi-dsi.c
+++ drivers/gpu/drm/bridge/synopsys/dw-mipi-dsi.c
@@ -814,9 +814,6 @@ static void dw_mipi_dsi_bridge_post_disable(struct drm_bridge *bridge)
 	struct dw_mipi_dsi *dsi = bridge_to_dsi(bridge);
 	const struct dw_mipi_dsi_phy_ops *phy_ops = dsi->plat_data->phy_ops;
 
-	if (phy_ops->power_off)
-		phy_ops->power_off(dsi->plat_data->priv_data);
-
 	/*
 	 * Switch to command mode before panel-bridge post_disable &
 	 * panel unprepare.
@@ -832,6 +829,9 @@ static void dw_mipi_dsi_bridge_post_disable(struct drm_bridge *bridge)
 	 * needs to be updated to manage our own call chains...
 	 */
 	dsi->panel_bridge->funcs->post_disable(dsi->panel_bridge);
+
+	if (phy_ops->power_off)
+		phy_ops->power_off(dsi->plat_data->priv_data);
 
 	if (dsi->slave) {
 		dw_mipi_dsi_disable(dsi->slave);
@@ -899,6 +899,9 @@ static void dw_mipi_dsi_mode_set(struct dw_mipi_dsi *dsi,
 
 	/* Switch to cmd mode for panel-bridge pre_enable & panel prepare */
 	dw_mipi_dsi_set_mode(dsi, 0);
+
+	if (phy_ops->power_on)
+		phy_ops->power_on(dsi->plat_data->priv_data);
 }
 
 static void dw_mipi_dsi_bridge_mode_set(struct drm_bridge *bridge,
@@ -915,15 +918,11 @@ static void dw_mipi_dsi_bridge_mode_set(struct drm_bridge *bridge,
 static void dw_mipi_dsi_bridge_enable(struct drm_bridge *bridge)
 {
 	struct dw_mipi_dsi *dsi = bridge_to_dsi(bridge);
-	const struct dw_mipi_dsi_phy_ops *phy_ops = dsi->plat_data->phy_ops;
 
 	/* Switch to video mode for panel-bridge enable & panel enable */
 	dw_mipi_dsi_set_mode(dsi, MIPI_DSI_MODE_VIDEO);
 	if (dsi->slave)
 		dw_mipi_dsi_set_mode(dsi->slave, MIPI_DSI_MODE_VIDEO);
-
-	if (phy_ops->power_on)
-		phy_ops->power_on(dsi->plat_data->priv_data);
 }
 
 static enum drm_mode_status
